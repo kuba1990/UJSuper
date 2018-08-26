2ba4c85e52e674387e3086aa86786e4c5cf89f9a
Jakub Wiśniowski <jakub.wisniowski@bisnode.com>
2018-08-26 10:43:44 +0200

    add

--- handson-demo-shopping-web/js/personslist.js
+++ handson-demo-shopping-web/js/personslist.js
@@ -9,14 +9,34 @@ var initPersonList = function() {
             $.each(data, function(index,countryPerson) {
                 console.log(countryPerson);
                 console.log("dupa");
+                var content = '<table id = "country_'+index+'>';
+
+
+               content +='<tr>'+
+                '<th colspan="5">Country</th>'+
+                    '</tr>'+
+                    '<tr>'+
+                    '<td>Id</td>'+
+                    '<td>Name</td>'+
+                    '<td>Last name</td>'+
+                '<td>Additional info</td>'+
+                '<td>Gender</td>'+
+                '</tr> ' ;
+
+                content += "</table>";
+
+                $('#country_'+index+'').append(content);
+
                 $.each(countryPerson, function(index1,person) {
-                    $('#country tr:last').after('<tr>\n' +
+
+
+                    /*$('#country tr:last').after('<tr>\n' +
                         '    <td>'+person.id+'</td>\n' +
                         '    <td>'+person.name+'</td>\n' +
                         '    <td>'+person.lastName+'</td>\n' +
                         '    <td>'+person.additonalInfo+'</td>\n' +
                         '    <td>'+person.gender+'</td>\n' +
-                        '  </tr>');
+                        '  </tr>');*/
 
                     /*console.log(person);
                     console.log("name: " + person.name);
