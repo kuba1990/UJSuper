87ea93235f855d9a361a6517664af8f29c51e63b
Jakub Wiśniowski <jakub.wisniowski@bisnode.com>
2018-08-26 10:22:26 +0200

    fix person

--- handson-demo-shopping-web/js/personslist.js
+++ handson-demo-shopping-web/js/personslist.js
@@ -1,5 +1,44 @@
 var initPersonList = function() {
-    var table = $('#personList').DataTable({
+    console.log("first line");
+    console.log(api_endpoint);
+    $.ajax({
+        type: 'GET',
+        url: api_endpoint + '/aggregateddata/ctry',
+        dataType: 'json',
+        success: function (data) {
+            $.each(data, function(index,countryPerson) {
+                console.log(countryPerson);
+                console.log("dupa");
+                $.each(countryPerson, function(index1,person) {
+                    $('#country tr:last').after('<tr>\n' +
+                        '    <td>'+person.id+'</td>\n' +
+                        '    <td>'+person.name+'</td>\n' +
+                        '    <td>'+person.lastName+'</td>\n' +
+                        '    <td>'+person.additonalInfo+'</td>\n' +
+                        '    <td>'+person.gender+'</td>\n' +
+                        '  </tr>');
+
+                    /*console.log(person);
+                    console.log("name: " + person.name);
+                    console.log("dupa2");*/
+
+
+                });
+
+            });
+           /* $.each(data, function(index, element) {
+                console.log(element);
+                console.log(index);
+                /!*$('body').append($('<div>', {
+                    text: element.name
+                }));*!/
+            });*/
+        }
+    });
+
+
+
+    /*var table = $('#personList').DataTable({
         ajax: {
             url: api_endpoint + '/aggregateddata/ctry',
             dataSrc: '_embedded.products'
@@ -9,18 +48,7 @@ var initPersonList = function() {
                 "fnCreatedCell": function (nTd, sData, oData, iRow, iCol) {
                     $(nTd).html("<a href='#/person/" + oData.id + "'>" + oData.name + "</a>");
                 }
-            },
-            { data: null,
-                "fnCreatedCell": function (nTd, sData, oData, iRow, iCol) {
-                    $(nTd).html("<button>Add to Cart</button>");
-                },
-                className: "dtCenter"
-            },
+            }
         ]
-    });
-
-    $('#personList tbody').on( 'click', 'button', function () {
-        var data = table.row( $(this).parents('tr') ).data();
-    //    addToCart(data);
-    } );
+    });*/
 };
